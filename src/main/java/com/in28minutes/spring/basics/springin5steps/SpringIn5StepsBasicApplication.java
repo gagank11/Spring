e328ada24f9e1ca00d16b83715d2d97678ed977e
gagan <gagancool1131@gmail.com>
2018-08-17 01:29:22 +0530

    organised the structure in basic and scope packages. example use of proxy in scope.

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,15 +1,20 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
+
+
+
 @SpringBootApplication
-public class SpringIn5StepsApplication {
+public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
 		
 		BinarySearchImpl binarySearchImpl=applicationContext.getBean(BinarySearchImpl.class);
 
